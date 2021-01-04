1ad8c73673447eaab88832040af1ce7283590af3
mrogovets <34492317+mrogovets@users.noreply.github.com>
2021-01-04 19:43:03 +0200

    calculator v1

---
+++ calc_v1.py
@@ -0,0 +1,17 @@
+"""Doc."""
+
+
+# Calculator v.1
+
+what = input("What do You want to do? (+, -): ")
+a = float(input("Input 1-st component: "))
+b = float(input("Input 2-d component: "))
+
+if what == "+":
+    c = a + b
+    print("Result: " + str(c))
+elif what == "-":
+    c = a - b
+    print("Result: " + str(c))
+else:
+    print("Invalid operation")
